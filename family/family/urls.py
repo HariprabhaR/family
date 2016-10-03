bd48457a907555a7561b789c2820e69d7da85f9c
Hariprabha R <hariprabhabca@gmail.com>
2016-10-03 21:30:06 +0100

    home page created

--- family/family/urls.py
+++ family/family/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+#from django.contrib import admin
+from home_family import views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'family.views.home', name='home'),
+    url(r'^$', 'home_family.views.home'),
+  
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
